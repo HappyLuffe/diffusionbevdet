7e5643a229fd2a9d3cbf0dbdcb76e11fbc0ccc2a
yudianyu <yudianyu@outlook.com>
2023-05-10 17:19:50 +0800

    5/10

--- configs/diffusionbev/diffusionbev_kitti_L.py
+++ configs/diffusionbev/diffusionbev_kitti_L.py
@@ -5,7 +5,7 @@ _base_ = [
 
 
 # evaluation
-evaluation = dict(interval=1)
+evaluation = dict(interval=10)
 # # optimizer
 # optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
 # optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
